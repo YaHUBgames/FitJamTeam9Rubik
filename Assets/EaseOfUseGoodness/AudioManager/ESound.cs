a179570e68a57d6fd326934fa73efcfd450ab448
logiis <65508531+logiis@users.noreply.github.com>
2023-04-08 13:26:07 +0200

    b
    b

--- Assets/EaseOfUseGoodness/AudioManager/ESound.cs
+++ Assets/EaseOfUseGoodness/AudioManager/ESound.cs
@@ -4,5 +4,34 @@ public enum ESound
     NoSound = -1,
     Default = 0,
 
-    Sound1 = 10
+    MenuClick = 10,
+    
+
+    DiceThrow = 20,
+    DiceFleshImpact,
+    DiceWallImpact,
+    DiceFloorImpact,
+    DicePickUp,
+    DiceLost,
+
+    PickipKey = 50,
+    PicupCard,
+    PickupChip,
+    Pickup,
+
+
+    LocatorKey = 60,
+    LocatorCad,
+
+
+    OpenDoorWithKey = 70,
+    OpenDoorWithCard,
+    OpenMainDoor,
+
+    GuardStep = 80,
+    GuardAgroStart,
+    GuardAgroStop,
+
+    SonarPing = 90
+    
 }
