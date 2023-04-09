835a73bd36cb61747e8bba5712eaa66fc2ec8232
savlisam <samuel.svalich@seznam.cz>
2023-04-09 11:05:28 +0200

    final commit

--- Assets/svalisam/TitleMenu.cs
+++ Assets/svalisam/TitleMenu.cs
@@ -8,6 +8,11 @@ public class TitleMenu : MonoBehaviour
     // Start is called before the first frame update
     public void Play()
     {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Tutorial()
+    {
         SceneManager.LoadScene(1);
     }
 
